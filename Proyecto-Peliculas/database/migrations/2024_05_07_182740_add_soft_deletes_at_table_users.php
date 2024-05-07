54db5ea4aac1f54c2eb943de59189c25b125f6b4
Jose Garcia <jose.garcia0857@alumnos.udg.mx>
2024-05-07 12:32:07 -0600

    Borrado Lógico implementado en tabla de usuarios

---
+++ Proyecto-Peliculas/database/migrations/2024_05_07_182740_add_soft_deletes_at_table_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //me equivoqué, en una migracion anterior y tengo que borrar primero esta columna antes
+        //de implementar el borrado logico correctamente
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('deleted_at'); //me equivoqué, en una migracion anterior y tengo que borrar primero esta columna antes de implementar
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
